bf92ab10e1cfd8edf4736a7153f9cdad07fc897a
Manuel <naruto06@mail.ru>
2017-02-28 12:55:50 +0300

    Helpers added

--- CoreKPI/Helpers.swift
+++ CoreKPI/Helpers.swift
@@ -7,7 +7,21 @@
 //
 
 import Foundation
+import UIKit
 
-struct NotificationNames {
+//Helpers
+struct NotificationNames
+{
     static let userTappedSecuritySwitch = "UserTappedSecuritySwitch"
+}
+
+struct UserDefaultsKeys
+{
+    static let pinCode = "PinCode"
+    static let token = "token"
+}
+
+struct OurColors
+{
+    static let violet = UIColor(red: 124.0/255.0, green: 77.0/255.0, blue: 255.0/255.0, alpha: 1.0)
 }
