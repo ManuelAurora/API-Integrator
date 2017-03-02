1907da8bc95764478b892a0970f1f7e5cd7f32f9
sem51 <oss.smichrissoft@gmail.com>
2017-03-02 13:01:31 +0300

    add payPal test method

--- CoreKPI/ExternalServices/PayPal.swift
+++ CoreKPI/ExternalServices/PayPal.swift
@@ -9,16 +9,33 @@
 import Foundation
 
 class PayPal: ExternalRequest {
-    func getBalance(success: @escaping () -> (), failure: @escaping failure ) {
+    
+    let payPalUri = "https://api.sandbox.paypal.com/"
+    
+    func getBalance(success: @escaping () -> (), failure: @escaping failure)  {
         
-        let url = "https://api.sandbox.paypal.com/v1/payments/payment"
+        let payUrl = payPalUri + "v1/payments/payment"
         let headers = ["Authorization" : "Bearer \(oauthToken)", "Content-Type" : "application/json"]
         let params: [String : Any] = ["intent" : "sale", "payer":["payment_method":"paypal"],"redirect_urls":["return_url":"http://corekpi.com", "cancel_url" : "http://example.com/your_cancel_url.html"], "transactions" : [["amount":["total":"7.47", "currency" : "USD"]]]]
         
-        self.getJson(url: url, header: headers, params: params, method: .post, success: { json in
+        self.getJson(url: payUrl, header: headers, params: params, method: .post, success: { json in
             success()
         }, failure: {error in
             failure(error)
         })
+    }
+    
+    func getInvoices(success: @escaping () -> (), failure: @escaping failure)  {
+        
+        let invoiceListUrl = payPalUri + "v1/invoicing/invoices/"
+        let headers = ["Authorization" : "Bearer \(oauthToken)", "Content-Type" : "application/json"]
+        let params: [String : Any] = [:]
+        
+        self.getJson(url: invoiceListUrl, header: headers, params: params, method: .post, success: { json in
+            success()
+        }, failure: {error in
+            failure(error)
+        })
+
     }
 }
