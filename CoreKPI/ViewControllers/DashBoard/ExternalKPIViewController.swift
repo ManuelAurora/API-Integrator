664bb77372768fce6781b54776ebf2d657e8fdb9
Manuel Aurora <Manuel@Ms-Mac-mini.local>
2017-03-16 11:16:41 +0300

    Merged with Dev

--- CoreKPI/ViewControllers/DashBoard/ExternalKPIViewController.swift
+++ CoreKPI/ViewControllers/DashBoard/ExternalKPIViewController.swift
@@ -86,7 +86,7 @@ class ExternalKPIViewController: OAuthViewController {
             showAlert(title: "Sorry!", message: "First you should select one or more KPI")
         }
     }
-
+    
     func showAlert(title: String, message: String) {
         let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
